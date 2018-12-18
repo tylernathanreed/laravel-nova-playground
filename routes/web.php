c6ff1e3fe0dc6820ea5b6ce83d95b6bd6aad9694
Tyler Reed <tylernathanreed@gmail.com>
2018-12-18 08:28:24 -0600

    ~ Morning 12/18/18

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,11 @@ Route::get('dropdown', function() {
 	return '
 		<link href="' . asset('css/app.css') . '" rel="stylesheet">
 		<link href="' . asset('vendor/dropdown/bootstrap.css') . '" rel="stylesheet">
-		<link href="' . asset('vendor/dropdown/bootstrap-theme.css') . '" rel="stylesheet">
 		<style>
 			body { margin: 20px; }
+			.w-dropdown {
+				width: 200px;
+			}
 			.dropdown-submenu {
 				position: relative;
 			}
@@ -42,28 +44,41 @@ Route::get('dropdown', function() {
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="' . asset('vendor/dropdown/bootstrap.js') . '" defer></script>
 		<div class="bg-30 hover:bg-40 mr-3 rounded">
-			<div class="dropdown relative">
+			<div class="relative">
 				<button class="px-3" type="button" data-toggle="dropdown">
 					Tutorials
 					<span class="caret"></span>
 				</button>
-				<ul class="dropdown-menu">
-					<li class="cursor-pointer text-80 hover:text-primary hover:bg-30 p-3 flex items-center no-underline">
-						<a tabindex="-1" href="#">HTML</a>
+				<ul class="dropdown-menu absolute pin-l float-left w-dropdown mt-1 list-reset bg-white border-20 rounded shadow-md">
+					<li>
+						<a tabindex="-1" href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">HTML</a>
 					</li>
-					<li class="cursor-pointer text-80 hover:text-primary hover:bg-30 p-3 flex items-center no-underline">
-						<a tabindex="-1" href="#">CSS</a>
+					<li>
+						<a tabindex="-1" href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">CSS</a>
 					</li>
-					<li class="dropdown-submenu cursor-pointer text-80 hover:text-primary hover:bg-30 p-3 flex items-center no-underline">
-						<a class="test" tabindex="-1" href="#">New dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a tabindex="-1" href="#">2nd level dropdown</a></li>
-							<li><a tabindex="-1" href="#">2nd level dropdown</a></li>
-							<li class="dropdown-submenu">
-								<a class="test" href="#">Another dropdown <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">3rd level dropdown</a></li>
-									<li><a href="#">3rd level dropdown</a></li>
+					<li class="h-px my-2 overflow-hidden bg-50"></li>
+					<li class="dropdown-submenu cursor-pointer hover:text-primary">
+						<a tabindex="-1" href="#" class="test block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">
+							New dropdown <span class="caret"></span>
+						</a>
+						<ul class="dropdown-menu absolute pin-l float-left w-dropdown mt-1 list-reset bg-white border-20 rounded shadow-md">
+							<li>
+								<a tabindex="-1" href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">2nd level dropdown</a>
+							</li>
+							<li class="disabled">
+								<a tabindex="-1" href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">2nd level dropdown</a>
+							</li>
+							<li class="dropdown-submenu cursor-pointer hover:text-primary">
+								<a href="#" class="test block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">
+									Another dropdown <span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu absolute pin-l float-left w-dropdown mt-1 list-reset bg-white border-20 rounded shadow-md">
+									<li>
+										<a href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">3rd level dropdown</a>
+									</li>
+									<li>
+										<a href="#" class="block p-4 cursor-pointer text-black hover:text-primary whitespace-no-wrap no-underline outline-none hover:bg-30">3rd level dropdown</a>
+									</li>
 								</ul>
 							</li>
 						</ul>
